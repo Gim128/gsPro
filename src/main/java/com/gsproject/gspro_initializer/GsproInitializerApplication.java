817a3d31d9c5f31d395756d833164e1426ee6fde
Gim128 <gimhanpabasara4@gmail.com>
2024-12-27 03:45:02 +0530

    gspro initialized

---
+++ src/main/java/com/gsproject/gspro_initializer/GsproInitializerApplication.java
@@ -0,0 +1,15 @@
+package com.gsproject.gspro_initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsproInitializerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GsproInitializerApplication.class, args);
+		System.out.println("Application is running");
+	}
+
+}
---
+++ src/test/java/com/gsproject/gspro_initializer/GsproInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsproject.gspro_initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsproInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
